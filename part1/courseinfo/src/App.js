4fc65620cf6a40ead00ca765d386b232eeac7e1c
Enrique Verea <kikeverea@gmail.com>
2022-07-14 00:04:03 +0200

    Step 3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,20 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  
   const parts = [part1, part2, part3]
-  const exercises = [exercises1, exercises2, exercises3]
 
   return (
     <div>
       <Header courseName={course} />
-      <Content parts={parts} exercises={exercises} />
-      <Total exercises={exercises}/>
+      <Content parts={parts} />
+      <Total parts={parts}/>
     </div>
   )
 }
@@ -27,9 +32,9 @@ const Header = (props) => (
 
 const Content = (props) => (
   <>
-    <Part part={props.parts[0]} exercise={props.exercises[0]}></Part>
-    <Part part={props.parts[1]} exercise={props.exercises[1]}></Part>
-    <Part part={props.parts[2]} exercise={props.exercises[2]}></Part>
+    <Part part={props.parts[0].name} exercise={props.parts[0].exercises}></Part>
+    <Part part={props.parts[1].name} exercise={props.parts[1].exercises}></Part>
+    <Part part={props.parts[2].name} exercise={props.parts[2].exercises}></Part>
   </>
 )
 
@@ -41,13 +46,12 @@ const Part = (props) => (
 
 const Total = (props) => (
   <>
-    <p>Number of exercises {addExercises(props.exercises)}</p>
+    <p>Number of exercises {sumExercises(props.parts)}</p>
   </>
 )
 
-const addExercises = (exercises) => (
-  exercises.reduce((total, numOfExercises) => total + numOfExercises)
-
+const sumExercises = (exercises) => (
+  exercises.reduce((total, part) => total + part.exercises, 0)
 )
 
 export default App
