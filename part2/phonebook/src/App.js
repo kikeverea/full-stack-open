ab326ac1701190ad672dc5f75b8b2acf501c7192
Enrique Verea <kikeverea@gmail.com>
2022-07-16 01:04:17 +0200

    phonebook: Step 1
    Functionality to add names to the phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,24 +3,59 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
+  ])
+
   const [newName, setNewName] = useState('')
+
+  const onNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addNewName = (event) => {
+    event.preventDefault();
+
+    if (!newName.trim())
+      return;
+
+    const personToAdd = {
+      name: newName
+    }
+    setPersons(persons.concat(personToAdd));
+    setNewName('');
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewName}>
         <div>
-          name: <input />
+          name: 
+            <input 
+              value={newName} 
+              onChange={onNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <PhoneBook persons={persons} />
     </div>
   )
 }
+
+// const PersonForm = () =>
+//   <form>
+//     <div>
+//       name: <Input value={newName} onChange={onInputChange}/>
+//     </div>
+//     <div>
+//       <button type="submit">add</button>
+//     </div>
+//   </form>
+
+const PhoneBook = ({persons}) =>
+  persons.map(person =>
+    <p key={person.name}>{person.name}</p>)
 
 export default App
