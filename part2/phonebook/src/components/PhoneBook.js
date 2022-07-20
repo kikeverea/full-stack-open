28440d4a8b8187217c05d1c80e2f61371aa290f7
Enrique Verea <kikeverea@gmail.com>
2022-07-21 01:03:51 +0200

    phonebook: refactor PhoneBook

--- part2/phonebook/src/components/PhoneBook.js
+++ part2/phonebook/src/components/PhoneBook.js
@@ -1,51 +1,51 @@
 import Person from './Person'
 import personService from '../services/personService'
 
-const PhoneBook = ({persons, onPersonsChange}) =>
+const PhoneBook = ({persons, onPersonsChange}) => {
+  
+  const deletePerson = (person, persons, setPersons) => {
+    const doDelete = window.confirm(`Delete ${person.name} ?`)
+    if (doDelete) {
+      personService
+        .remove(person)
+        .then(success => {
+          if (success) 
+            setPersons(removePersonFromList(persons, person))
+        })
+        .catch(error => {
+          setPersons(removePersonFromList(persons, person))
+        })
+    }
+  }
+  
+  const removePersonFromList = (persons, remove) =>
+    persons.filter(person => person.id !== remove.id)
 
-  persons.length === 0
+  return (
+    persons.length === 0
     ? <p>No phones listed</p>
     : <table>
-        <PhoneBookHead />
-        <PersonList persons={persons} onPersonsChange={onPersonsChange} />
+        <PhoneBookHeader />
+        <tbody>
+          {persons.map(person => 
+            <Person key={person.name} 
+                    name={person.name} 
+                    phone={person.number}
+                    onDelete={()=>
+                          deletePerson(person, persons, onPersonsChange)
+                    } />
+          )}
+        </tbody>
     </table>
+  )
+}
 
-const PhoneBookHead = () =>
+const PhoneBookHeader = () => 
   <thead>
     <tr>
       <th>Name</th>
       <th>Phone number</th>
     </tr>
   </thead>
-
-const PersonList = ({persons, onPersonsChange}) => 
-    <tbody>
-      {persons.map(person => 
-        <Person key={person.name} 
-                name={person.name} 
-                phone={person.number}
-                onDelete={()=>
-                      deletePerson(person, persons, onPersonsChange)
-                } />
-      )}
-    </tbody>
-
-const deletePerson = (person, persons, setPersons) => {
-  const doDelete = window.confirm(`Delete ${person.name} ?`)
-  if (doDelete) {
-    personService
-      .remove(person)
-      .then(success => {
-        if (success) 
-          setPersons(removePersonFromList(persons, person))
-      })
-      .catch(error => {
-        setPersons(removePersonFromList(persons, person))
-      })
-  }
-}
-
-const removePersonFromList = (persons, remove) =>
-  persons.filter(person => person.id !== remove.id)
 
 export default PhoneBook
