432edca9540eb274ce8434a6bcbd970600c616bd
Enrique Verea <kikeverea@gmail.com>
2022-08-05 00:07:23 +0200

    Initial commit

---
+++ part4/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      // eslint-disable-next-line no-undef
+      (process.platform === 'win32' ? 'windows' : 'unix')
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ]
+  }
+}
