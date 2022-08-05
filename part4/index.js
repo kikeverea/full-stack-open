a6356ee172fe6ff21247000f35a6b47aeac0ec74
Enrique Verea <kikeverea@gmail.com>
2022-08-05 02:18:28 +0200

    Move routing into controllers/blogs

--- part4/index.js
+++ part4/index.js
@@ -11,23 +11,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
