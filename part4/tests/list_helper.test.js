c2725f0221e7bb976f72964ee472e9d6de84707f
Enrique Verea <kikeverea@gmail.com>
2022-08-05 18:23:11 +0200

    Step 2: define and test totalLikes() helper

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,8 +1,25 @@
 const listHelper = require('../utils/list_helper')
+const blogs = require('./dummyBlogs')
 
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  test('when list is empty, equals 0', () => {
+    expect(listHelper.totalLikes([])).toBe(0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const listWithOneBlog = [blogs[0]]
+    const loneBlogLikes = listWithOneBlog[0].likes
+    expect(listHelper.totalLikes(listWithOneBlog)).toBe(loneBlogLikes)
+  })
+
+  test('when list has many blogs, equals the likes of all blogs', () => {
+    expect(listHelper.totalLikes(blogs)).toBe(36)
+  })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (total, blog) =>
+    total + blog.likes
+
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
