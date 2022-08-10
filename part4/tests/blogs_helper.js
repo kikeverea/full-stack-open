97dc1169690a9943f13fe33569ad240f643cc2d7
Enrique Verea <kikeverea@gmail.com>
2022-08-10 20:58:57 +0200

    Rename blogs in blogs_helper to initialBlogs

--- part4/tests/blogs_helper.js
+++ part4/tests/blogs_helper.js
@@ -1,6 +1,6 @@
 const Blog = require('../models/blog')
 
-const blogs = [
+const initialBlogs = [
   {
     _id: '5a422a851b54a676234d17f7',
     title: 'React patterns',
@@ -57,6 +57,6 @@ const blogsInDb = async () => {
 }
 
 module.exports = {
-  blogs,
+  blogs: initialBlogs,
   blogsInDb
 }
