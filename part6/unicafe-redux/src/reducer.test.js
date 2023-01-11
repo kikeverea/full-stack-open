f9bf4e6280ec244482191d7c5205548f374be3ad
Enrique Verea <kikeverea@gmail.com>
2023-01-11 01:43:25 +0100

    Unicafe with redux: 6.1
    Implement the functionality required by the store

--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -8,8 +8,15 @@ describe('unicafe reducer', () => {
     bad: 0
   }
 
+  const testAction = (action, expectedState) => {
+    const state = initialState
+    deepFreeze(state)
+
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual(expectedState)
+  }
+
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
     const action = {
       type: 'DO_NOTHING'
     }
@@ -19,17 +26,48 @@ describe('unicafe reducer', () => {
   })
 
   test('good is incremented', () => {
-    const action = {
-      type: 'GOOD'
-    }
-    const state = initialState
+    testAction(
+      { type: 'GOOD' },
+      {
+        good: 1,
+        ok: 0,
+        bad: 0
+      })
+  })
 
-    deepFreeze(state)
-    const newState = counterReducer(state, action)
-    expect(newState).toEqual({
-      good: 1,
-      ok: 0,
-      bad: 0
-    })
+  test('ok is incremented', () => {
+    testAction(
+      { type: 'OK' },
+      {
+        good: 0,
+        ok: 1,
+        bad: 0
+      })
+  })
+
+  test('bad is incremented', () => {
+    testAction(
+      { type: 'BAD' },
+      {
+        good: 0,
+        ok: 0,
+        bad: 1
+      })
+  })
+
+  test('zero resets feedbacks to "0"', () => {
+    testAction(
+      { type: 'ZERO' },
+      {
+        good: 0,
+        ok: 0,
+        bad: 0
+      })
+  })
+
+  test('invalid action returns current state', () => {
+    testAction(
+      { type: 'INVALID' },
+      initialState)
   })
 })
