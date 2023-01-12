9d94e67c94df3cc5f8a2daf8e579ee187ded53b6
Enrique Verea <kikeverea@gmail.com>
2023-01-12 11:40:17 +0100

    Redux anecdotes, step 6: 6.8
    Separate the anecdote list into a component called AnecdoteList

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -12,10 +12,14 @@ const AnecdoteForm = () => {
   }
 
   return (
-    <form onSubmit={ createNewAnecdote }>
-      <div><input name='anecdote'/></div>
-      <button type='submit'>create</button>
-    </form>)
+    <>
+      <h2>create new</h2>
+      <form onSubmit={ createNewAnecdote }>
+        <div><input name='anecdote'/></div>
+        <button type='submit'>create</button>
+      </form>
+    </>
+  )
 }
 
 export default AnecdoteForm
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,30 @@
+import { useSelector } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  const vote = (id) => {
+    dispatch(voteAnecdote(id))
+  }
+
+  return (
+    anecdotes.map(anecdote =>
+      <div
+        key={anecdote.id}
+        style={{ display: 'flex', flexDirection: 'column', gap: 5, padding: '8px 0 8px 0' }}>
+        <div>
+          {anecdote.content}
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row', gap: 10 }}>
+          has {anecdote.votes}
+          <button onClick={() => vote(anecdote.id)}>vote</button>
+        </div>
+      </div>
+    )
+  )
+}
+
+export default AnecdoteList
