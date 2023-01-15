9cc716818aefddae58cad712106553df92adf837
Enrique Verea <kikeverea@gmail.com>
2023-01-16 00:43:50 +0100

    Redux-anecdotes, with backend: 6.13
    Pull the initial app state from server

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -21,8 +12,11 @@ const asObject = (anecdote) => {
 
 const anecdotesSlice = createSlice({
   name: 'anecdotes',
-  initialState: initialState.map(asObject),
+  initialState: [],
   reducers: {
+    setAnecdotes(state, action) {
+      return action.payload
+    },
     voteAnecdote(state, action) {
       const voteAnecdoteId = action.payload
       return state.map(
@@ -38,5 +32,5 @@ const anecdotesSlice = createSlice({
   }
 })
 
-export const { voteAnecdote, createAnecdote } = anecdotesSlice.actions
+export const { voteAnecdote, createAnecdote, setAnecdotes } = anecdotesSlice.actions
 export default anecdotesSlice.reducer
