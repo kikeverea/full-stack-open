41618906fa826b1e3cad7c1cbc5a6c5258cf8311
Enrique Verea <kikeverea@gmail.com>
2023-01-16 15:11:19 +0100

    Redux-anecdotes, grand finale: 6.21
    Fix Notification bug (successive notifications cancel each other)

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -2,24 +2,31 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const notificationSlice = createSlice({
   name: 'notification',
-  initialState: '',
+  initialState: { content: '', id: 0 },
   reducers: {
     setNotification(state, action) {
       return action.payload
     },
     clearNotification() {
-      return ''
+      return { content: '', id: 0 }
     }
   }
 })
 
-export const showNotification = (notification, duration) => {
-  return async dispatch => {
-    dispatch(setNotification(notification))
+export const showNotification = (content, duration) => {
+  return async (dispatch, getState) => {
 
-    setTimeout(() => {
+    const notificationOnDisplay = getState().notification
+
+    if (notificationOnDisplay) {
+      clearTimeout(notificationOnDisplay.id)
+    }
+
+    const id = setTimeout(() => {
       dispatch(clearNotification())
     }, duration * 1000)
+
+    dispatch(setNotification({ content, id }))
   }
 }
 
