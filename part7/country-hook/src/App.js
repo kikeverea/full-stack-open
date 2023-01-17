9759e35b178ccf308e0dbfe2535fc4539268e45a
Enrique Verea <kikeverea@gmail.com>
2023-01-17 14:56:38 +0100

    Country hook: 7.7
    Implement useCountry hook, with useEffect()

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -18,7 +18,17 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (name) {
+      axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+        .then(response => {
+          const { name, population, capital, flags } = response.data[0]
+          setCountry({ name, population, capital, flags })
+        })
+        .catch(() => setCountry(null))
+    }
+    else setCountry(null)
+  }, [name])
 
   return country
 }
@@ -31,11 +41,11 @@ const Country = ({ country }) => {
   return (
     <div>
       <h3>{country.name.common}</h3>
-      <div>population {country.population}</div> 
+      <div>population {country.population}</div>
       <div>capital {country.capital}</div>
-      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
+      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/>
     </div>
-  )  
+  )
 }
 
 const App = () => {
