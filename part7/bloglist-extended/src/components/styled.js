3994070676885280b4799f2bcc1c816afed27be0
Enrique Verea <kikeverea@gmail.com>
2023-01-23 19:41:46 +0100

    Add styled components

---
+++ part7/bloglist-extended/src/components/styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const TableColumn = styled.td`
+    padding: 10px;
+  `
+
+export const TableHeader = styled.th`
+    padding: 10px 20px 10px 20px;
+  `
+
+export const FlexRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 16px;
+  `
